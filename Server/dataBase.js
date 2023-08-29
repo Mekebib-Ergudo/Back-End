e674e5e8c44d4a73b3d76feba87a3fd829139e5f
Mekebib Ergudo <ergudomekebib95@gmail.com>
2023-08-28 22:42:05 -0400

    Db deployed

--- Server/dataBase.js
+++ Server/dataBase.js
@@ -1,23 +1,27 @@
 const mysql = require("mysql2");
-
 // make connection with our dataBase
-const pool = mysql.createPool({
-	host: process.env.Host,
-	user: process.env.user_Name,
-	database: process.env.Db_Name,
-	password: process.env.Password,
-	waitForConnections: true,
-	connectionLimit: 10,
-	port: process.env.Db_Port,
-	socket: process.env.Db_Socket,
-});
+
+// const pool = mysql.createPool({
+// 	host: process.env.Host,
+// 	user: process.env.user_Name,
+// 	database: process.env.Db_Name,
+// 	password: process.env.Password,
+// 	waitForConnections: true,
+// 	connectionLimit: 10,
+// 	port: process.env.Db_Port,
+// 	socket: process.env.Db_Socket,
+// });
+
+// Planet Db
+
+const pool = mysql.createConnection(process.env.DATABASE_URL);
 
 //  create connection with our Db
-pool.getConnection((err, connection) => {
-	if (err) console.log(err);
 
-	console.log("Db COnnected..!");
-});
+// pool.getConnection((err, connection) => {
+// 	if (err) console.log(err);
+// 	console.log("Db COnnected..!");
+// });
 
 // create Tables...
 let registration = `CREATE TABLE if not exists registration (
@@ -30,23 +34,21 @@ let profile = `CREATE TABLE  if not exists profile (
     user_profile_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    last_name VARCHAR(255) NOT NULL
 )`;
 let question = `CREATE TABLE if not exists question (
     question_id INT PRIMARY KEY AUTO_INCREMENT,
     question VARCHAR(255) NOT NULL,
     question_description VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    user_id INT NOT NULL
+    
 )`;
 let answer = `CREATE TABLE  if not exists answer (
     answer_id INT PRIMARY KEY AUTO_INCREMENT,
     answer VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
-    question_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES registration(user_id),
-    FOREIGN KEY (question_id) REFERENCES question(question_id)
+    question_id INT NOT NULL
+    
 )`;
 
 pool.query(registration, (err, result) => {
